4e946cae82e520f6ef063149579c7fdb44778b79
jackbrbell <jack.bell@digital.hmrc.gov.uk>
2018-01-22 11:41:15 +0000

    updat greet function

--- src/main/scala/app/Greeting.scala
+++ src/main/scala/app/Greeting.scala
@@ -1,8 +1,9 @@
 package app
 
+import scala.io.StdIn
+
 object Greeting extends App {
   def greet(name: String) = println(s"hello $name")
-val name: String = "Jack"
-  println("hello" + name)
-  greet("Jack")
+val name: String = StdIn.readLine("What is your name?")
+  greet(name)
 }
